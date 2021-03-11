5911b85b7c78ae563b859162ab0ab03945273aa4
Léo Claude <leo.claude@yahoo.com>
2021-03-11 15:46:45 +0100

    TD à jour

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+function addTodoTag(textTodo) {
+
+    const todoTag = document.createElement('div');
+    const todoText = document.createElement('p');
+    const todoButton = document.createElement('button');
+
+    todoTag.className = "todo";
+    todoButton.className = "todo-button";
+
+    todoText.innerHTML = textTodo;
+    todoButton.innerHTML = "×";
+    todoTag.appendChild(todoText);
+    todoTag.appendChild(todoButton);
+
+    todoButton.addEventListener('click', function() {
+        this.parentNode.remove();
+    }, false);
+
+    document.querySelector('.todo-list').appendChild(todoTag);
+}
+
+document.addTodoTab.addEventListener('submit', function(e) {
+    e.preventDefault();
+    addTodoTag(this.todoText.value);
+});
